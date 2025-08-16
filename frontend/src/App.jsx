631ee45dca3bb1508958cd7ed9b3b0e68b9e30f8
viraj992 <it21183836@my.sliit.lk>
2025-08-16 21:54:02 +0530

    Add frontend and restructure project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="w-full h-screen bg-blue-500">
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
