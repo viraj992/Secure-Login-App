82a0fee920ff4ca1f3c7819395df07cdaab0e889
viraj992 <it21183836@my.sliit.lk>
2025-08-17 12:37:03 +0530

    remove unwanted files & folders🚮

--- routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createUser, loginUser } from '../controllers/userController.js';
-
-const userRouter = express.Router();
-userRouter.post("/",createUser)
-userRouter.post("/login", loginUser)
-
-
-export default userRouter; // still day 05
