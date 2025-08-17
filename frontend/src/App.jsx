8bf8a76e846b29aca1f3d37f9199618ee5aa4285
viraj992 <it21183836@my.sliit.lk>
2025-08-17 13:06:25 +0530

    Needed File strucure Added ✏️

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
-
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
+import RegisterPage from "./pages/registerPage";
+import AdminPage from "./pages/adminPage";
 function App() {
   
 
   return (
-    <>
-      <div className="w-full h-screen bg-blue-500">
-        Hello hi
-      </div>
-    </>
-  )
+    <BrowserRouter>
+        <div className="w-full h-screen flex justify-center items-center bg-amber-300">
+          <Routes path="/">
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path = "/admin/*" element={<AdminPage/>}/>
+              </Routes>
+        </div>
+
+    </BrowserRouter>
+  );
 }
 
 export default App
